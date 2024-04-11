679c375c147354db0f132e64ef94dbc7e3533fba
rastafasha <mercadocreativo@gmail.com>
2024-04-11 18:01:55 -0400

    update sidebar

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,14 +4,22 @@ export const environment = {
     // url_servicios: 'http://127.0.0.1:8000/api',
     // url_frontend: 'http://localhost:4300/',
     // url_media: 'http://127.0.0.1:8000/storage/',
+    
     //remoto v2
     // url_backend: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/',
     // url_servicios: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/public/api',
     // url_frontend: 'https://citasmedicasv1.malcolmcordova.com/',
     // url_media: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/storage/app/public/',
+    
     //remoto cliente trial
     url_backend: 'https://csdleondeccs.malcolmcordova.com/backend-api-citas/',
     url_servicios: 'https://csdleondeccs.malcolmcordova.com/backend-api-citas/public/api',
     url_frontend: 'https://csdleondeccs.malcolmcordova.com/',
     url_media: 'https://csdleondeccs.malcolmcordova.com/backend-api-citas/storage/app/public/',
+
+    //remoto demo  dominio
+    // url_backend: 'https://clinica.health-connect.me/backend-api-citas/',
+    // url_servicios: 'https://clinica.health-connect.me/backend-api-citas/public/api',
+    // url_frontend: 'https://clinica.health-connect.me/',
+    // url_media: 'https://clinica.health-connect.me/backend-api-citas/storage/app/public/',
 };
